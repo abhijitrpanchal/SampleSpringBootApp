b18b24ef805529b8a23b51f4fd4e7b9ccb81faba
abhijit.panchal <abhijit.panchal@accenture.com>
2017-01-30 12:52:59 +0530

    New code

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,12 +1,15 @@
 package hello;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SpringBootApplication
 public class HelloController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value = "/hello}",method = RequestMethod.GET)
 	public String index() {
 		return "Greetings from Spring Boot";
 	}
